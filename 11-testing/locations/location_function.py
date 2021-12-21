3580152f349983003738c990d1b2e4dd259ca738
Bulyka Anton <ant_ilich@mail.ru>
2021-12-21 22:16:07 +0200

    к функцие get_city_country добавлен параметр population добавлен тест с этим параметром

--- 11-testing/locations/location_function.py
+++ 11-testing/locations/location_function.py
@@ -5,15 +5,15 @@
 #   1.0     2021
 # ---------------------------------------------
 
-def get_city_country(country: str, city: str) -> str:
+def get_city_country(country: str, city: str, population: int = 5_000_000) -> str:
     """
     Форматированная строка вида "Город, Страна"
     :param country: (str) country
     :param city: (str) city
     :return: (str) "City, Country"
     """
-    string_line = f'{city}, {country}'
-    return string_line.title()
+    string_line = f'{city.title()}, {country.title()} - population {population}'
+    return string_line
 
 
 def main():
--- 11-testing/tests/test_locations.py
+++ 11-testing/tests/test_locations.py
@@ -4,18 +4,27 @@
 #   Ver.    Date
 #   1.0     2021
 # ---------------------------------------------
+
 import unittest
 from ..locations.location_function import get_city_country
 
-class locations_testcase(unittest.TestCase):
+
+class LocationsTestCase(unittest.TestCase):
     """Тесты для 'get_city_country'"""
+    
     def test_city_country(self):
-        """
-        Строка вида 'Santiago, Chile'
-        :return:
-        """
+        """Строка вида 'Santiago, Chile'"""
         formatted_location = get_city_country('chile', 'santiago')
-        self.assertEqual(formatted_location, 'Santiago, Chile')
+        self.assertEqual(formatted_location,
+                         'Santiago, Chile - population 5000000')
+    
+    def test_city_country_population(self):
+        """Строка вида 'Santiago, Chile- population 5000000' """
+        formatted_location = get_city_country('chile', 'santiago',
+                                              population=5000000)
+        self.assertEqual(formatted_location,
+                         'Santiago, Chile - population 5000000')
+
 
 def main():
     unittest.main()
