45c7c5d8c79d58d48a5af7cceac8155361f34601
Bulyka Anton <ant_ilich@mail.ru>
2021-12-21 16:00:57 +0200

    init

---
+++ 11-testing/name_function.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------
+#   Program by Anton B.
+#
+#   Ver.    Date
+#   1.0     2021
+# ---------------------------------------------
+
+
+def get_formatted_name(first, last):
+    """Строит отформатированное полное имя."""
+    full_name = f"{first} {last}"
+    return full_name.title()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
