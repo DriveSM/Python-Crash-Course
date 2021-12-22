d030d834864ed284ad70831047fac83e530c4c29
Bulyka Anton <ant_ilich@mail.ru>
2021-12-22 20:53:02 +0200

    AI добавлен файл корабль откорректирован основной модуль для выводв корабля

--- project-alien-invasion/alien_invasion.py
+++ project-alien-invasion/alien_invasion.py
@@ -10,6 +10,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -23,6 +24,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+
+        self.ship = Ship(self)
         
         # Назначение цвета фона.
         self.bg_color = self.settings.bg_color
@@ -37,6 +40,7 @@ class AlienInvasion:
             
             # При каждом проходе цикла перерисовывается экран.
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
             
             # Отображение последнего прорисованног экрана.
             pygame.display.flip()
--- project-alien-invasion/settings.py
+++ project-alien-invasion/settings.py
@@ -13,7 +13,7 @@ class Settings:
         # Параметры экрана
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (77, 77, 0)
+        self.bg_color = (0, 59, 89)
 
 
 def main():
