1104e446bf891e6b39db5f4e4b517c4efc7f1c94
Bulyka Anton <ant_ilich@mail.ru>
2021-12-22 19:58:20 +0200

    добавлен файл настроек откорректирован основной модуль

--- project-alien-invasion/alien_invasion.py
+++ project-alien-invasion/alien_invasion.py
@@ -9,6 +9,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -16,10 +18,15 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
+        self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
         
+        # Назначение цвета фона.
+        self.bg_color = self.settings.bg_color
+    
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
@@ -27,8 +34,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.bg_color)
+            
             # Отображение последнего прорисованног экрана.
             pygame.display.flip()
+
 
 def main():
     # Создание экземпляра и запуск игры.
---
+++ project-alien-invasion/settings.py
@@ -0,0 +1,24 @@
+# ---------------------------------------------
+#   Program by Anton B.
+#
+#   Ver.    Date
+#   1.0     2021
+# ---------------------------------------------
+
+class Settings:
+    """Класс для хранения всех настроек игры Alien Invasion."""
+    
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (77, 77, 0)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
